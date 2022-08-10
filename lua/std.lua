c45efe1902c9f49e3e428a01971d1b83cbd3179b
Jahor Laryn <mrojahor@gmail.com>
2022-08-10 22:08:02 +0300

    Add str funcs

--- lua/std.lua
+++ lua/std.lua
@@ -14,4 +14,27 @@ function export.max(a, b)
     return b
 end
 
+function export.strsplit(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t={}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        table.insert(t, str)
+    end
+    return t
+end
+
+function export.strjoin(list, delimiter)
+    local len = #list
+    if len == 0 then
+        return "" 
+    end
+    local string = list[1]
+    for i = 2, len do 
+        string = string .. delimiter .. list[i] 
+    end
+    return string
+end
+
 return export
